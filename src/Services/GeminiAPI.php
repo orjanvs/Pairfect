b4254011b8b6fde03dd6e4d90fcd5e8b844671dc
orjanvs <stuelandrjan@gmail.com>
2025-10-09 10:14:16 +0200

    Fixed prompt

--- src/Services/GeminiAPI.php
+++ src/Services/GeminiAPI.php
@@ -10,18 +10,15 @@ function enhanceWithGemini(string $food, string $pairingText): string
 {
 $client = new Client($_ENV['GEMINI_API_KEY']);
 
-$prompt = 
-    "You're a sommelier that provides wine pairing suggestions based on food items.
-    The user suggests a food item, then you use the response from the Spoonacular API to provide a better and more friendly
-    answer. Don't give a specific wine brand, just the type of wine that pairs well with the food item. 
-    If you don't know the answer, just say that you don't know. Do not try to make up an answer.
-    If the the user asks for something that is not a food item, 
-    politely inform them that you can only provide wine pairings for food items.
-    If the question is empty, ask the user to provide a food item.
-    Keep the response under 100 words.
-    Also do short responses to everyday talk like 'hello', 'how are you', 'what's your name' etc.
+$prompt = <<<TXT
+    INSTRUCTION: 
+    - Enhance the following wine pairing suggestion by making it more engaging and informative.
+    - Use a max of 100 words.
+    - Don't include special characters or formatting such as *. Use plain text only.
+    - Give only specific recommendations of a wine if it is found in vinmonopolet.no, don't include article numbers or prices.
 
-    DATA: Food item: $food, Source info: $pairingText";
+    DATA: Food item: $food, Source info: $pairingText"
+    TXT;
 
 $response = $client->generativeModel("gemini-2.5-flash")
     ->generateContent(
